7c6b895395f13650f9ff0d65d50a7febd239c04b
Michael <michaelbrockus@gmail.com>
2021-03-22 09:52:14 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// file: main.cpp
+// author: Michael Brockus
+// gmail: <michaelbrockus@gmail.com>
+//
+#include <iostream>
+#include <stdlib>
+
+// main is where program execution starts
+int main()
+{
+    std::cout << "Hello, Coding World!" << std::endl;
+    return EXIT_SUCCESS;
+} // end of function main
